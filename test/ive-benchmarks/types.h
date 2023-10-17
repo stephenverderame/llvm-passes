8c2b12e957eb235e2580a643a0b50287b66281b3
sev <stephenverderame@gmail.com>
2023-10-16 22:38:23 -0400

    Add benchmarks

---
+++ test/ive-benchmarks/types.h
@@ -0,0 +1,22 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+#define LEN 32000
+#define LEN2 256
+
+#ifndef TYPE
+#define TYPE float
+#define FABS(x) fabsf(x)
+#else
+#define FABS(x) fabs(x)
+#endif
+
+#ifndef X_TYPE
+#define X_TYPE TYPE
+#endif
+
+#ifndef ALIGNMENT
+#define ALIGNMENT 16
+#endif
+
+#endif  // TYPES_H
